8ae5c9319ce7f2d05e7e71b634b9052a89eaba1d
wdqyb <29684101@qq.com>
2015-06-12 17:55:03 +0800

    修改ext信息发送方法

--- app/src/main/java/com/xuejian/client/lxp/module/toolbox/StrategyVisitedListActivity.java
+++ app/src/main/java/com/xuejian/client/lxp/module/toolbox/StrategyVisitedListActivity.java
@@ -23,6 +23,8 @@ import com.aizou.core.widget.prv.PullToRefreshBase;
 import com.aizou.core.widget.prv.PullToRefreshListView;
 import com.easemob.EMCallBack;
 import com.google.gson.reflect.TypeToken;
+import com.lv.Listener.SendMsgListener;
+import com.lv.im.IMClient;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.umeng.analytics.MobclickAgent;
@@ -75,7 +77,7 @@ public class StrategyVisitedListActivity extends PeachBaseActivity {
     StrategyAdapter mStrategyListAdapter;
     boolean isShare;
     int mCurrentPage = 0;
-    int chatType;
+    String chatType;
     String toId;
     private StrategyBean originalStrategy;
     private String userId;
@@ -244,7 +246,7 @@ public class StrategyVisitedListActivity extends PeachBaseActivity {
 
     private void initData() {
         toId = getIntent().getStringExtra("toId");
-        chatType = getIntent().getIntExtra("chatType", 0);
+        chatType = getIntent().getStringExtra("chatType");
 //        getStrategyListData(0);
 //        mMyStrategyLv.doPullRefreshing(true, 100);
         setupViewFromCache();
@@ -386,7 +388,7 @@ public class StrategyVisitedListActivity extends PeachBaseActivity {
                             @Override
                             public void onDialogShareOk(Dialog dialog, int type, String content) {
                                 DialogManager.getInstance().showLoadingDialog(mContext);
-                                IMUtils.sendExtMessage(mContext, type, content, chatType, toId, new EMCallBack() {
+                                IMClient.getInstance().sendExtMessage(toId,chatType,content,type,new SendMsgListener() {
                                     @Override
                                     public void onSuccess() {
                                         DialogManager.getInstance().dissMissLoadingDialog();
@@ -395,11 +397,10 @@ public class StrategyVisitedListActivity extends PeachBaseActivity {
                                                 ToastUtil.getInstance(mContext).showToast("已发送~");
                                             }
                                         });
-
                                     }
 
                                     @Override
-                                    public void onError(int i, String s) {
+                                    public void onFailed(int code) {
                                         DialogManager.getInstance().dissMissLoadingDialog();
                                         runOnUiThread(new Runnable() {
                                             public void run() {
@@ -407,11 +408,6 @@ public class StrategyVisitedListActivity extends PeachBaseActivity {
 
                                             }
                                         });
-
-                                    }
-
-                                    @Override
-                                    public void onProgress(int i, String s) {
 
                                     }
                                 });
