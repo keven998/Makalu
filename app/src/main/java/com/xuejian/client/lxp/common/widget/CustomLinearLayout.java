eb0e70c2fd99b1b560e078347b9bb5bb867c6cba
Arya <yefengwch@126.com>
2015-09-10 16:45:29 +0800

    add file

---
+++ app/src/main/java/com/xuejian/client/lxp/common/widget/CustomLinearLayout.java
@@ -0,0 +1,256 @@
+package com.xuejian.client.lxp.common.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.OverScroller;
+
+import com.xuejian.client.lxp.R;
+
+/**
+ * Created by xuyongchen on 15/9/10.
+ */
+public class CustomLinearLayout extends LinearLayout{
+
+    private View mTop;
+
+    private int mTopViewHeight;
+    private ViewGroup mInnerScrollView;
+    private boolean isTopHidden = false;
+
+    private OverScroller mScroller;
+    private VelocityTracker mVelocityTracker;
+    private int mTouchSlop;
+    private int mMaximumVelocity, mMinimumVelocity;
+
+    private float mLastY;
+    private boolean mDragging;
+
+    private boolean isInControl = false;
+    public CustomLinearLayout(Context context){
+        super(context);
+        setOrientation(LinearLayout.VERTICAL);
+
+        mScroller = new OverScroller(context);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mMaximumVelocity = ViewConfiguration.get(context)
+                .getScaledMaximumFlingVelocity();
+        mMinimumVelocity = ViewConfiguration.get(context)
+                .getScaledMinimumFlingVelocity();
+    }
+
+    public CustomLinearLayout(Context context,AttributeSet attrs){
+        super(context,attrs);
+        setOrientation(LinearLayout.VERTICAL);
+
+        mScroller = new OverScroller(context);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mMaximumVelocity = ViewConfiguration.get(context)
+                .getScaledMaximumFlingVelocity();
+        mMinimumVelocity = ViewConfiguration.get(context)
+                .getScaledMinimumFlingVelocity();
+    }
+
+    public CustomLinearLayout(Context context,AttributeSet attrs,int defStyle){
+        super(context, attrs, defStyle);
+        setOrientation(LinearLayout.VERTICAL);
+
+        mScroller = new OverScroller(context);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mMaximumVelocity = ViewConfiguration.get(context)
+                .getScaledMaximumFlingVelocity();
+        mMinimumVelocity = ViewConfiguration.get(context)
+                .getScaledMinimumFlingVelocity();
+    }
+
+
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mTop = findViewById(R.id.user_info_pannel);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+      //  ViewGroup.LayoutParams params = mViewPager.getLayoutParams();
+      //  params.height = getMeasuredHeight() - mNav.getMeasuredHeight();
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mTopViewHeight = mTop.getMeasuredHeight();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        float y = ev.getY();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dy = y - mLastY;
+
+              /*  if (!isInControl && c != null && c.getTop() == 0 && isTopHidden
+                        && dy > 0) {
+                    isInControl = true;
+                    ev.setAction(MotionEvent.ACTION_CANCEL);
+                    MotionEvent ev2 = MotionEvent.obtain(ev);
+                    dispatchTouchEvent(ev);
+                    ev2.setAction(MotionEvent.ACTION_DOWN);
+                    return dispatchTouchEvent(ev2);
+                }*/
+
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = ev.getAction();
+        float y = ev.getY();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dy = y - mLastY;
+                if (Math.abs(dy) > mTouchSlop) {
+                    mDragging = true;
+                   // View c = lv.getChildAt(lv.getFirstVisiblePosition());
+                    /*if (!isTopHidden || (c != null && c.getTop() == 0 && isTopHidden && dy > 0)) {
+                        initVelocityTrackerIfNotExists();
+                        mVelocityTracker.addMovement(ev);
+                        mLastY = y;
+                        return true;
+                    }*/
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                mDragging = false;
+                recycleVelocityTracker();
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        initVelocityTrackerIfNotExists();
+        mVelocityTracker.addMovement(event);
+        int action = event.getAction();
+        float y = event.getY();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if (!mScroller.isFinished())
+                    mScroller.abortAnimation();
+                mLastY = y;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float dy = y - mLastY;
+
+                Log.e("TAG", "dy = " + dy + " , y = " + y + " , mLastY = " + mLastY);
+
+                if (!mDragging && Math.abs(dy) > mTouchSlop) {
+                    mDragging = true;
+                }
+                if (mDragging) {
+                    scrollBy(0, (int) -dy);
+
+                    // 如果topView隐藏，且上滑动时，则改变当前事件为ACTION_DOWN
+                    if (getScrollY() == mTopViewHeight && dy < 0) {
+                        event.setAction(MotionEvent.ACTION_DOWN);
+                        dispatchTouchEvent(event);
+                        isInControl = false;
+                    }
+                }
+
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                mDragging = false;
+                recycleVelocityTracker();
+                if (!mScroller.isFinished()) {
+                    mScroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mDragging = false;
+                mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+                int velocityY = (int) mVelocityTracker.getYVelocity();
+                if (Math.abs(velocityY) > mMinimumVelocity) {
+                    fling(-velocityY);
+                }
+                recycleVelocityTracker();
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    public void fling(int velocityY) {
+        mScroller.fling(0, getScrollY(), 0, velocityY, 0, 0, 0, mTopViewHeight);
+        invalidate();
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        if (y < 0) {
+            y = 0;
+        }
+        if (y > mTopViewHeight) {
+            y = mTopViewHeight;
+        }
+        if (y != getScrollY()) {
+            super.scrollTo(x, y);
+        }
+
+        isTopHidden = getScrollY() == mTopViewHeight;
+
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(0, mScroller.getCurrY());
+            invalidate();
+        }
+    }
+
+    private void initVelocityTrackerIfNotExists() {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    private void recycleVelocityTracker() {
+        if (mVelocityTracker != null) {
+            mVelocityTracker.recycle();
+            mVelocityTracker = null;
+        }
+    }
+
+
+
+}
